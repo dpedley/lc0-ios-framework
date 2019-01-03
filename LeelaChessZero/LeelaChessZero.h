30078024d3f5cfc8fad152565a1b5640bb0e0ed4
Doug Pedley <dpedley@gmail.com>
2019-01-02 16:41:07 -0800

    Initial Commit

---
+++ LeelaChessZero/LeelaChessZero.h
@@ -0,0 +1,19 @@
+//
+//  LeelaChessZero.h
+//  LeelaChessZero
+//
+//  Created by Douglas Pedley on 1/2/19.
+//  Copyright © 2019 d0. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LeelaChessZero.
+FOUNDATION_EXPORT double LeelaChessZeroVersionNumber;
+
+//! Project version string for LeelaChessZero.
+FOUNDATION_EXPORT const unsigned char LeelaChessZeroVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeelaChessZero/PublicHeader.h>
+
+
